76d93563cf06a2010675da8cdf00ff48b434de1b
nurik-web-eng <n41038333@gmail.com>
2026-08-27 15:01:46 +0500

    Update app.py

--- app.py
+++ app.py
@@ -141,4 +141,5 @@ if __name__ == '__main__':
     print("🚰 Сервер доставки воды запущен!")
     print("📍 http://localhost:5000")
     print(" Уведомления отправляются в VK")
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
